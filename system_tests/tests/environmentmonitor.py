fd36f5a53c9f70433e6dd355cfb3067fef4d2415
Jack Doughty <jackb.doughty@btinternet.com>
2024-08-05 12:46:15 +0100

    run safe ruff fixes

--- system_tests/tests/environmentmonitor.py
+++ system_tests/tests/environmentmonitor.py
@@ -1,13 +1,12 @@
 import unittest
+
 from parameterized import parameterized
 
 # Tests for IOC
-
 from utils.channel_access import ChannelAccess
-from utils.ioc_launcher import get_default_ioc_dir, ProcServLauncher
+from utils.ioc_launcher import ProcServLauncher, get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, parameterized_list
-
+from utils.testing import get_running_lewis_and_ioc, parameterized_list, skip_if_recsim
 
 DEVICE_PREFIX = "ENVMON_01"
 
